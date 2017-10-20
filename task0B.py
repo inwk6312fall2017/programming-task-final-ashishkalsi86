dc14f921d9e69b7154d93e6a143722e8aaefe35f
ASHISH KALSI <ashishkalsi86@24dcaea7d349>
2017-10-20 18:24:44 +0000

    weather

---
+++ task0B.py
@@ -0,0 +1,46 @@
+from weather
+import Weather
+weather = Weather()
+
+
+
+location = weather.lookup_by_location('halifax')
+condition = location.condition()
+print (condition['text'])
+
+
+all_days=[]
+
+i=0
+for forecasts in location.forecast():
+    if i<5:
+        daily = []
+        daily.append(forecasts['text'])
+        daily.append(forecasts['date'])
+        daily.append(forecasts['high'])
+        daily.append(forecasts['low'])
+        all_days.append(daily)
+        i+=1
+
+print(all_days)
+
+maxT=0
+for day in all_days:
+    if int(day[2])>int(maxT):
+        maxT=day[2]
+        that_day = day[1]
+print("The hottest day is on "+that_day +' and the temparature is '+ maxT)
+
+least=10000
+for day in all_days:
+    if int(day[3])<int(least):
+        least=day[3]
+        that_day=day[1]
+print("The coldest day is on "+that_day+' and the tempearature is '+least)
+
+for day in all_days:
+    if day[0]=='Cloudy':
+        that_day=day[1]
+        print("The chances that it may rain on "+day[1]+" is more since it is "+day[0])
+    else:
+        pass
